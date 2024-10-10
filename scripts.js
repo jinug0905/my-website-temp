ff7fa0f0fd0f65448967ced62f203055c0404323
jinug0905 <60326817+jinug0905@users.noreply.github.com>
2024-10-10 18:03:28 -0500

    4. put more korean scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 document.getElementById("fun-button").addEventListener("click", function() {
     const facts = [
         "제가 그 유미쒸 핫걸이에유",
+        "대단하쥬",
     ];
     const randomFact = facts[Math.floor(Math.random() * facts.length)];
     document.getElementById("fun-fact").innerText = randomFact;
