60784925a361ad0d3e02a9b00ba9472b9f97519b
jinug0905 <60326817+jinug0905@users.noreply.github.com>
2024-10-10 17:59:53 -0500

    3. Update into Korean scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,6 @@
 document.getElementById("fun-button").addEventListener("click", function() {
     const facts = [
-        "Did you know? Potatoes were the first food to be grown in space!",
-        "Fun fact: Potatoes can be used to generate electricity in simple science experiments.",
-        "Potatoes are 80% water!",
+        "제가 그 유미쒸 핫걸이에유",
     ];
     const randomFact = facts[Math.floor(Math.random() * facts.length)];
     document.getElementById("fun-fact").innerText = randomFact;
